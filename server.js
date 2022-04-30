00248635dc573157607470248f0bcdf27879a95e
Skeith <skeith@asterialabs.io>
2022-04-30 18:07:01 +0800

    Add basic health check endpoint

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const logger = require('./helpers/logger');
+const { version } = require('./package.json');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK', version });
+});
 
 app.listen(config.APP.PORT, () => {
   logger.info(`Server is running on ${config.APP.PORT}`);
